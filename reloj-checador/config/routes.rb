02a6af516207da79cf19c3203cf6994c8582d20d
Adrián <adrianramaaguilar@gmail.com>
2021-02-03 23:33:52 -0400

    Devise for admin login no registration added

--- reloj-checador/config/routes.rb
+++ reloj-checador/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, :controllers => { sessions: "admins/sessions",
+                                        registrations: "admins/registrations",
+                                        passwords: "admins/passwords",
+                                        confirmations: "admins/confirmations" }
   resources :companies
+
+  root to: redirect("/companies")
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
