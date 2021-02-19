6a0fd6f95f8d2a26cdbcae72210fccd048fda9ba
Adrián <ramasoftdev@gmail.com>
2021-02-18 21:29:04 -0400

    adding in Welcome controller validation for redirection to attendance new

--- reloj-checador/config/routes.rb
+++ reloj-checador/config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
+  root "welcome#index"
+
   devise_for :admins, :controllers => { sessions: "admins/sessions",
                                         registrations: "admins/registrations",
                                         passwords: "admins/passwords",
                                         confirmations: "admins/confirmations" }
-  resources :companies
 
-  root "welcome#index"
-  get '/welcome', to: 'welcome#index'
-  get '/checkin', to: 'checkins#index'
-  resources :attendaces, except: [:edit, :delete]
+  resources :companies
   resources :companies
   resources :employees
 
+  resources :attendaces, except: [:edit, :delete]
+
+  get "/welcome", to: "welcome#index"
+  get "/checkin", to: "checkins#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
