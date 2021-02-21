bfa736b458865b29be1b72f3f32686cbb8b6f2fb
Adrián <ramasoftdev@gmail.com>
2021-02-21 14:29:26 -0400

    Changing fontawesome`s packages and fixing company`s names in employees index view

--- reloj-checador/app/javascript/packs/application.js
+++ reloj-checador/app/javascript/packs/application.js
@@ -5,27 +5,17 @@
 import "jquery";
 import "popper.js"
 import "bootstrap";
-import "@fortawesome/fontawesome-free/css/all";
 import "channels";
+import "@fortawesome/fontawesome-free/css/all"
 
 import * as ActiveStorage from "@rails/activestorage";
 
-import { dom, library } from '@fortawesome/fontawesome-svg-core'
-
 import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
-import { faTwitter } from '@fortawesome/free-brands-svg-icons'
-import { fas } from '@fortawesome/free-solid-svg-icons'
 import toastr from 'toastr';
-// import "../stylesheets/application.scss";
-
-
-
 
 Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
 
 window.toastr = toastr;
-
-library.add(fas, faTwitter);
