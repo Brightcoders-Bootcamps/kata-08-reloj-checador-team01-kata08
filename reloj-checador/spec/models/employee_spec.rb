86fee67fe293d151626ac33c47c781e7c183d01a
Mario Mancilla <mariormt17@gmail.com>
2021-02-22 17:00:01 -0600

    Employee and Attendance tests added

--- reloj-checador/spec/models/employee_spec.rb
+++ reloj-checador/spec/models/employee_spec.rb
@@ -1,5 +1,23 @@
-require 'rails_helper'
+require '../rails_helper'
 
 RSpec.describe Employee, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is not valid without email attribute" do
+    expect(Employee.new(:email => "")).to_not be_valid
+  end
+
+  it "is not valid without name attribute" do
+    expect(Employee.new(:name => "")).to_not be_valid
+  end
+
+  it "is not valid without lastname attribute" do
+    expect(Employee.new(:lastname => "")).to_not be_valid
+  end
+
+  it "is not valid without position attribute" do
+    expect(Employee.new(:position => "")).to_not be_valid
+  end
+
+  it "is not valid without private number attribute" do
+    expect(Employee.new(:private_number => "")).to_not be_valid
+  end
 end
