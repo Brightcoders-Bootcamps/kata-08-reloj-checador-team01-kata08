4d266fb4494d0d77b97cd3001cd05a25607a9b99
FranckZnt19 <frania.sinahi.suarez.lopez@google.com>
2021-02-23 00:46:20 -0600

    add test company model
    add two test for validated the attributes name and address and one test for create a new company

--- reloj-checador/spec/models/company_spec.rb
+++ reloj-checador/spec/models/company_spec.rb
@@ -1,5 +1,15 @@
 require '../rails_helper'
 
 RSpec.describe Company, type: :model do
+  it "is not valid without name attribute" do
+    expect(Company.new(:name => nil)).to_not be_valid
+  end
+  it "is not valid without address attribute" do
+    expect(Company.new(:address => nil)).to_not be_valid
+  end
+  describe "submit" do
+    @comp = Company.new(:name => "mycompani", :address=>"myaddresstest 5")
+    @comp.submit
+  end
   
 end
