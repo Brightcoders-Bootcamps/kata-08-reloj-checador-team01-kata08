0badc7dc82473c5a9dada993282f880cb31f1ecb
Mario Mancilla <mariormt17@gmail.com>
2021-02-25 12:48:39 -0600

    Tests updated on Attendance and Employee spec

--- reloj-checador/spec/models/attendace_spec.rb
+++ reloj-checador/spec/models/attendace_spec.rb
@@ -1,7 +1,16 @@
 require '../rails_helper'
 
 RSpec.describe Attendace, type: :model do
+  subject {
+    described_class.new(private_number: "12345")
+  }
+
+  it "is valid with all attributes" do
+    expect(subject).to be_valid
+  end
+
   it "is not valid without private number attribute" do
-    expect(Attendace.new(:private_number => "")).to_not be_valid
+    subject.private_number = nil
+    expect(subject).to_not be_valid
   end
 end
--- reloj-checador/spec/models/employee_spec.rb
+++ reloj-checador/spec/models/employee_spec.rb
@@ -1,23 +1,40 @@
 require '../rails_helper'
 
 RSpec.describe Employee, type: :model do
+  subject {
+    described_class.new(email: "mail@example.com",
+                        name: "Random name",
+                        lastname: "Random lastname",
+                        position: "Random position",
+                        private_number: "12345")
+  }
+
+  it "is valid with all attributes" do
+    expect(subject).to be_valid
+  end
+
   it "is not valid without email attribute" do
-    expect(Employee.new(:email => "")).to_not be_valid
+    subject.email = nil
+    expect(subject).to_not be_valid
   end
 
   it "is not valid without name attribute" do
-    expect(Employee.new(:name => "")).to_not be_valid
+    subject.name = nil
+    expect(subject).to_not be_valid
   end
 
   it "is not valid without lastname attribute" do
-    expect(Employee.new(:lastname => "")).to_not be_valid
+    subject.lastname = nil
+    expect(subject).to_not be_valid
   end
 
   it "is not valid without position attribute" do
-    expect(Employee.new(:position => "")).to_not be_valid
+    subject.position = nil
+    expect(subject).to_not be_valid
   end
 
   it "is not valid without private number attribute" do
-    expect(Employee.new(:private_number => "")).to_not be_valid
+    subject.private_number = nil
+    expect(subject).to_not be_valid
   end
 end
